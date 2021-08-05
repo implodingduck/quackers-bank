29d2f2087e0fb024184baa7c87d59f521f84e341
implodingduck <zsek320@gmail.com>
2021-08-05 12:58:46 -0500

    adding a springboot app with a react front end

---
+++ src/main/java/com/quackers/bank/security/AADOAuth2LoginSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.quackers.bank.security;
+
+import com.azure.spring.aad.webapp.AADWebSecurityConfigurerAdapter;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AADOAuth2LoginSecurityConfig extends AADWebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+        // Do some custom configuration
+    }  
+}
