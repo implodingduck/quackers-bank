4088ee70280066dec86927f696a9b7c1f790cb79
implodingduck <zsek320@gmail.com>
2021-08-06 11:11:05 -0500

    lets try this again

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ resource "azurerm_app_service_plan" "aspjar" {
 }
 
 resource "azurerm_container_registry" "test" {
-  name                = "acr-${local.func_name}"
+  name                = "acr0${local.func_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Standard"
