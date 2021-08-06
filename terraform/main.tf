8c4890b395314fed3940ab2a964b7d2a0240f862
implodingduck <zsek320@gmail.com>
2021-08-06 13:34:18 -0500

    lets try passing in with -D

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,7 +68,7 @@ resource "null_resource" "publish_jar"{
   }
   provisioner "local-exec" {
     working_dir = "../"
-    command     = "./mvnw package azure-webapp:deploy"
+    command     = "./mvnw package azure-webapp:deploy -Darm.resourcegroup=${azurerm_resource_group.rg.name} -Darm.region=${azurerm_resource_group.rg.location}"
   }
 }
 
