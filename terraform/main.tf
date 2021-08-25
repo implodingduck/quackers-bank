ed8c995e4086f470a682cbc13de0efe00fcf4bb9
implodingduck <zsek320@gmail.com>
2021-08-25 12:37:04 -0500

    fixing location typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -205,7 +205,7 @@ resource "azurerm_mssql_firewall_rule" "editor" {
 resource "azurerm_storage_account" "sa" {
   name                     = "sa${local.func_name}"
   resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_kind             = "StorageV2"
   account_tier             = "Standard"
   account_replication_type = "LRS"
