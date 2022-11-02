7b5c8ac2c50249ba82a77bc55af93e5648c33ab0
implodingduck <zsek320@gmail.com>
2022-11-02 16:09:24 -0500

    using cluster_name

--- terraform/main.tf
+++ terraform/main.tf
@@ -152,7 +152,7 @@ resource "azurerm_application_insights" "app" {
 }
 
 resource "azurerm_key_vault" "kv" {
-  name                       = "kv-${local.func_name}"
+  name                       = "kv-${local.cluster_name}"
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
