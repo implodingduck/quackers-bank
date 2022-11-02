96367ec12f706ffd6b603077efd4de4283160d01
implodingduck <zsek320@gmail.com>
2022-11-02 13:55:59 -0500

    adding acr

--- terraform/main.tf
+++ terraform/main.tf
@@ -122,3 +122,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
   tags = local.tags
 
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "acr${local.cluster_name}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = false
+}
