05a28d75973b09c516adc3ce842aa8864f564bf8
implodingduck <zsek320@gmail.com>
2022-11-02 13:16:00 -0500

    adding in aks

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 
   subscription_id = var.subscription_id
 }
@@ -48,4 +52,73 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-${local.gh_repo}-${random_string.unique.result}-${local.loc_for_naming}"
   location = var.location
   tags = local.tags
+}
+
+
+
+resource "azurerm_virtual_network" "default" {
+  name                = "${local.cluster_name}-vnet-eastus"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.7.0.0/16"]
+
+  tags = local.tags
+}
+
+
+resource "azurerm_subnet" "default" {
+  name                 = "default-subnet-eastus"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.7.0.0/24"]
+}
+
+resource "azurerm_subnet" "cluster" {
+  name                 = "${local.cluster_name}-subnet-eastus"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.7.2.0/23"]
+
+}
+
+data "azurerm_kubernetes_service_versions" "current" {
+  location = azurerm_resource_group.rg.location
+}
+
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                    = local.cluster_name
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  dns_prefix              = local.cluster_name
+  kubernetes_version      = data.azurerm_kubernetes_service_versions.current.latest_version
+  private_cluster_enabled = false
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_B2ms"
+    os_disk_size_gb = "128"
+    vnet_subnet_id  = azurerm_subnet.cluster.id
+
+
+  }
+  network_profile {
+    network_plugin     = "azure"
+    network_policy     = "azure"
+    service_cidr       = "10.255.252.0/22"
+    dns_service_ip     = "10.255.252.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+  }
+
+  role_based_access_control_enabled = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+  
+  oidc_issuer_enabled = true
+  workload_identity_enabled = true
+
+  tags = local.tags
+
 }
