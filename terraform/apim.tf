b938e050a9f388b28852cdea0725bb0fe3681bab
implodingduck <zsek320@gmail.com>
2022-11-04 10:03:32 -0500

    lets add an apim

---
+++ terraform/apim.tf
@@ -0,0 +1,35 @@
+resource "azurerm_subnet" "apim" {
+  name                 = "apim-subnet-eastus"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.default.name
+  address_prefixes     = ["10.7.4.0/24"]
+}
+
+resource "azurerm_api_management" "apim" {
+  name                 = "apim-quackersbank${random_string.unique.result}"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  publisher_name       = "Implodingduck"
+  publisher_email      = "something@nothing.com"
+  virtual_network_type = "External"
+  virtual_network_configuration {
+    subnet_id = azurerm_subnet.apim.id
+  }
+  policy = [
+    {
+      xml_content = <<-EOT
+    <policies>
+      <inbound />
+      <backend>
+        <forward-request />
+      </backend>
+      <outbound />
+    </policies>
+EOT
+      xml_link    = null
+    },
+  ]
+  zones    = []
+  sku_name = "Developer_1"
+  tags     = local.tags
+}
