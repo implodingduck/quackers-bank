e7f6204b63e0fc14898376005f97ee12e220bd96
implodingduck <zsek320@gmail.com>
2022-11-16 12:13:21 -0600

    working on adding a helper function for start stop

--- terraform/function.tf
+++ terraform/function.tf
@@ -19,7 +19,7 @@ resource "azurerm_linux_function_app" "func" {
   name                = local.func_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-
+  service_plan_id     = azurerm_service_plan.asp.id
   storage_account_name            = azurerm_storage_account.sa.name
 
 
