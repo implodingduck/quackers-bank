dd43a3d7d31ab4480acf3c872a17769222f996e8
implodingduck <zsek320@gmail.com>
2022-11-28 14:47:10 -0600

    updating rev2

--- terraform/apim.tf
+++ terraform/apim.tf
@@ -90,7 +90,7 @@ resource "azurerm_api_management_api" "revisionv1" {
 }
 
 resource "azurerm_api_management_api" "revisionv2" {
-  name                 = "revision-api"
+  name                 = "revision-api;rev=2"
   resource_group_name  = azurerm_resource_group.rg.name
   api_management_name  = azurerm_api_management.apim.name
   revision             = "2"
