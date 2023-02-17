d806793f2e6240e905abef4939feddf5adc295fb
implodingduck <zsek320@gmail.com>
2023-02-17 13:20:54 -0600

    tweaks for new sub

--- terraform/main.tf
+++ terraform/main.tf
@@ -228,39 +228,6 @@ resource "azurerm_user_assigned_identity" "kvcsidriver" {
   name = "uai-kvcsidriver-${local.cluster_name}"
 }
 
-# Add back in when AzAPI supports OIDC
-# resource "azapi_resource" "fic" {
-#   depends_on = [
-#     azurerm_kubernetes_cluster.aks
-#   ]
-#   type = "Microsoft.ManagedIdentity/userAssignedIdentities/federatedIdentityCredentials@2022-01-31-preview"
-#   name      = "fic-kvcsidriver"
-#   parent_id = azurerm_user_assigned_identity.kvcsidriver.id
-
-#   body = jsonencode({
-#     properties = {
-#       audiences = [
-#         "api://AzureADTokenExchange"
-#       ]
-#       issuer = azurerm_kubernetes_cluster.aks.oidc_issuer_url 
-#       subject = "system:serviceaccount:quackersbank:${azurerm_user_assigned_identity.kvcsidriver.name}"
-#     }
-#   })
-# }
-
-
-# resource "null_resource" "fic" {
-#   depends_on = [
-#     azurerm_kubernetes_cluster.aks
-#   ]
-#   triggers = {
-#     index = azurerm_kubernetes_cluster.aks.oidc_issuer_url
-#   }
-#   provisioner "local-exec" {
-#     command     = "az identity federated-credential create --name fic-kvcsidriver --identity-name ${azurerm_user_assigned_identity.kvcsidriver.name} --resource-group ${azurerm_resource_group.rg.name} --issuer ${azurerm_kubernetes_cluster.aks.oidc_issuer_url } --subject system:serviceaccount:quackersbank:${azurerm_user_assigned_identity.kvcsidriver.name}"
-#   }
-# }
-
 resource "azurerm_federated_identity_credential" "fic-kvcsidriver" {
   name                = "fic-kvcsidriver"
   resource_group_name = azurerm_resource_group.rg.name
