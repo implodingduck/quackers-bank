130356e91bc38b981a7a82345d5da47f78453c14
implodingduck <zsek320@gmail.com>
2023-02-21 19:55:57 -0600

    ok next

--- terraform/main.tf
+++ terraform/main.tf
@@ -248,17 +248,17 @@ resource "azurerm_user_assigned_identity" "kvcsidriver" {
 #   subject             = "system:serviceaccount:quackersbank:${azurerm_user_assigned_identity.kvcsidriver.name}"
 # }
 
-resource "azurerm_role_assignment" "aks_role_assignemnt_msi" {
-  scope                             = azurerm_user_assigned_identity.kvcsidriver.id
-  role_definition_name              = "Managed Identity Operator"
-  principal_id                      = azurerm_user_assigned_identity.kvcsidriver.principal_id
-  skip_service_principal_aad_check  = true 
-}
+# resource "azurerm_role_assignment" "aks_role_assignemnt_msi" {
+#   scope                             = azurerm_user_assigned_identity.kvcsidriver.id
+#   role_definition_name              = "Managed Identity Operator"
+#   principal_id                      = azurerm_user_assigned_identity.kvcsidriver.principal_id
+#   skip_service_principal_aad_check  = true 
+# }
 
 
-resource "azurerm_role_assignment" "networkcontributor" {
-  scope                             = azurerm_virtual_network.default.id
-  role_definition_name              = "Network Contributor"
-  principal_id                      = azurerm_user_assigned_identity.kvcsidriver.principal_id
-  skip_service_principal_aad_check  = true 
-}
+# resource "azurerm_role_assignment" "networkcontributor" {
+#   scope                             = azurerm_virtual_network.default.id
+#   role_definition_name              = "Network Contributor"
+#   principal_id                      = azurerm_user_assigned_identity.kvcsidriver.principal_id
+#   skip_service_principal_aad_check  = true 
+# }
