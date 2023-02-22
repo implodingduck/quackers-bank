42d4d57db01c2ed629f8300a53cd226e22c1f805
implodingduck <zsek320@gmail.com>
2023-02-21 19:44:04 -0600

    forward without cluster

--- terraform/function.tf
+++ terraform/function.tf
@@ -35,7 +35,7 @@ resource "azurerm_linux_function_app" "func" {
     type         = "SystemAssigned"
   }
   app_settings = {
-    "CLUSTER_ID"                     = azurerm_kubernetes_cluster.aks.id  
+    "CLUSTER_ID"                     = "" #azurerm_kubernetes_cluster.aks.id  
     "ENABLE_ORYX_BUILD"              = "true"
     "SCM_DO_BUILD_DURING_DEPLOYMENT" = "1"
     "WEBSITE_MOUNT_ENABLED"          = "1"
@@ -80,5 +80,11 @@ resource "null_resource" "publish_func" {
 resource "azurerm_role_assignment" "system" {
   scope                = azurerm_kubernetes_cluster.aks.id
   role_definition_name = "Contributor"
+  principal_id         = azurerm_linux_function_app.func.identity.0.principal_id  
+}
+
+resource "azurerm_role_assignment" "system" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Azure Event Hubs Data Receiver"
   principal_id         = azurerm_linux_function_app.func.identity.0.principal_id  
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -92,63 +92,63 @@ data "azurerm_kubernetes_service_versions" "current" {
 }
 
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                    = "${local.cluster_name}"
-  location                = azurerm_resource_group.rg.location
-  resource_group_name     = azurerm_resource_group.rg.name
-  dns_prefix              = "${local.cluster_name}"
-  kubernetes_version      = data.azurerm_kubernetes_service_versions.current.latest_version
-  private_cluster_enabled = false
-  default_node_pool {
-    name            = "default"
-    node_count      = 1
-    vm_size         = "Standard_B4ms"
-    os_disk_size_gb = "128"
-    vnet_subnet_id  = azurerm_subnet.cluster.id
-    max_pods        = 60
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                    = "${local.cluster_name}"
+#   location                = azurerm_resource_group.rg.location
+#   resource_group_name     = azurerm_resource_group.rg.name
+#   dns_prefix              = "${local.cluster_name}"
+#   kubernetes_version      = data.azurerm_kubernetes_service_versions.current.latest_version
+#   private_cluster_enabled = false
+#   default_node_pool {
+#     name            = "default"
+#     node_count      = 1
+#     vm_size         = "Standard_B4ms"
+#     os_disk_size_gb = "128"
+#     vnet_subnet_id  = azurerm_subnet.cluster.id
+#     max_pods        = 60
 
-  }
-  network_profile {
-    network_plugin      = "azure"
-    network_plugin_mode = "Overlay"
-    pod_cidr            = "192.168.0.0/16"
-    service_cidr       = "10.255.252.0/22"
-    dns_service_ip     = "10.255.252.10"
-    docker_bridge_cidr = "172.17.0.1/16"
-  }
+#   }
+#   network_profile {
+#     network_plugin      = "azure"
+#     network_plugin_mode = "Overlay"
+#     pod_cidr            = "192.168.0.0/16"
+#     service_cidr       = "10.255.252.0/22"
+#     dns_service_ip     = "10.255.252.10"
+#     docker_bridge_cidr = "172.17.0.1/16"
+#   }
 
-  key_vault_secrets_provider {
-    secret_rotation_enabled = true
-    secret_rotation_interval = "5m"
-  }
+#   key_vault_secrets_provider {
+#     secret_rotation_enabled = true
+#     secret_rotation_interval = "5m"
+#   }
 
-  role_based_access_control_enabled = false
+#   role_based_access_control_enabled = false
 
-  identity {
-    type = "UserAssigned"
-    identity_ids = [
-      azurerm_user_assigned_identity.kvcsidriver.id
-    ]
-  }
-  kubelet_identity {
-    client_id = azurerm_user_assigned_identity.kvcsidriver.client_id
-    object_id = azurerm_user_assigned_identity.kvcsidriver.principal_id
-    user_assigned_identity_id = azurerm_user_assigned_identity.kvcsidriver.id
-  }
+#   identity {
+#     type = "UserAssigned"
+#     identity_ids = [
+#       azurerm_user_assigned_identity.kvcsidriver.id
+#     ]
+#   }
+#   kubelet_identity {
+#     client_id = azurerm_user_assigned_identity.kvcsidriver.client_id
+#     object_id = azurerm_user_assigned_identity.kvcsidriver.principal_id
+#     user_assigned_identity_id = azurerm_user_assigned_identity.kvcsidriver.id
+#   }
   
-  oidc_issuer_enabled = true
-  workload_identity_enabled = true
-  # microsoft_defender {
-  #   log_analytics_workspace_id = data.azurerm_log_analytics_workspace.default.id
-  # }
-  oms_agent {
-    log_analytics_workspace_id = data.azurerm_log_analytics_workspace.default.id
-  }
+#   oidc_issuer_enabled = true
+#   workload_identity_enabled = true
+#   # microsoft_defender {
+#   #   log_analytics_workspace_id = data.azurerm_log_analytics_workspace.default.id
+#   # }
+#   oms_agent {
+#     log_analytics_workspace_id = data.azurerm_log_analytics_workspace.default.id
+#   }
 
-  open_service_mesh_enabled = false
-  tags = local.tags
+#   open_service_mesh_enabled = false
+#   tags = local.tags
 
-}
+# }
 
 resource "azurerm_container_registry" "acr" {
   name                = "acr${local.cluster_name}"
@@ -159,11 +159,11 @@ resource "azurerm_container_registry" "acr" {
 }
 
 
-resource "azurerm_role_assignment" "acrpull_role" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
+# resource "azurerm_role_assignment" "acrpull_role" {
+#   scope                            = azurerm_container_registry.acr.id
+#   role_definition_name             = "AcrPull"
+#   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+# }
 
 resource "azurerm_application_insights" "app" {
   name                = "${local.cluster_name}-insights"
@@ -239,14 +239,14 @@ resource "azurerm_user_assigned_identity" "kvcsidriver" {
   name = "uai-kvcsidriver-${local.cluster_name}"
 }
 
-resource "azurerm_federated_identity_credential" "fic-kvcsidriver" {
-  name                = "fic-kvcsidriver"
-  resource_group_name = azurerm_resource_group.rg.name
-  audience            = ["api://AzureADTokenExchange"]
-  issuer              = azurerm_kubernetes_cluster.aks.oidc_issuer_url
-  parent_id           = azurerm_user_assigned_identity.kvcsidriver.id
-  subject             = "system:serviceaccount:quackersbank:${azurerm_user_assigned_identity.kvcsidriver.name}"
-}
+# resource "azurerm_federated_identity_credential" "fic-kvcsidriver" {
+#   name                = "fic-kvcsidriver"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   audience            = ["api://AzureADTokenExchange"]
+#   issuer              = azurerm_kubernetes_cluster.aks.oidc_issuer_url
+#   parent_id           = azurerm_user_assigned_identity.kvcsidriver.id
+#   subject             = "system:serviceaccount:quackersbank:${azurerm_user_assigned_identity.kvcsidriver.name}"
+# }
 
 resource "azurerm_role_assignment" "aks_role_assignemnt_msi" {
   scope                             = azurerm_user_assigned_identity.kvcsidriver.id
