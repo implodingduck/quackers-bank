d9dbc592c661b29ef865012664779dc6d67dfeac
implodingduck <zsek320@gmail.com>
2023-02-24 13:35:48 -0600

    lets try ..8.0/21

--- terraform/aca.tf
+++ terraform/aca.tf
@@ -3,7 +3,7 @@ resource "azurerm_subnet" "aca" {
   name                 = "aca-subnet-eastus"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.default.name
-  address_prefixes     = ["10.7.5.0/21"]
+  address_prefixes     = ["10.7.8.0/21"]
 
 }
 
