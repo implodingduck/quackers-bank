357ef632becacc3daad1f999c93f0ab2fbf659be
implodingduck <zsek320@gmail.com>
2023-03-06 11:51:54 -0600

    trying to get a tf spring app deployment

--- terraform/spring.tf
+++ terraform/spring.tf
@@ -4,3 +4,19 @@ resource "azurerm_spring_cloud_service" "this" {
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "B0"
 }
+
+resource "azurerm_spring_cloud_app" "frontend" {
+  name                = "frontend"
+  resource_group_name = azurerm_spring_cloud_service.this.resource_group_name
+  service_name        = azurerm_spring_cloud_service.this.name
+}
+
+resource "azurerm_spring_cloud_container_deployment" "frontend" {
+  name                = "frontend"
+  spring_cloud_app_id = azurerm_spring_cloud_app.frontend.id
+  instance_count      = 1
+  
+  server             = "ghcr.io"
+  image              = "quackers-bank-transactions-api:main"
+  language_framework = "springboot"
+}
