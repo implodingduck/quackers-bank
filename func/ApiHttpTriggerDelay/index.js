ec6953c4d08168248fea42e5e69e1a96a7d809d0
implodingduck <zsek320@gmail.com>
2023-06-01 11:36:09 -0500

    adding a delay function

---
+++ func/ApiHttpTriggerDelay/index.js
@@ -0,0 +1,19 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+    const delay = (req.query.delay || (req.body && req.body.delay));
+    const delayMS = delay ? parseInt(delay) * 1000 : 1000
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully... with a delay of " + delayMS + " ms"
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response... with a delay of " + delayMS + " ms";
+
+    context.log('Starting the pause...');
+    
+    await new Promise(resolve => setTimeout(resolve, delayMS));
+    context.log('Ending the pause...');
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
